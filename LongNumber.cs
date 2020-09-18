fa2857d51f2d92ba253458eca774312d7ff7449e
artandfi <artyom.fisunenko@gmail.com>
2020-09-18 13:55:31 +0300

    comparison, minus, implicit conversion

--- LongNumber.cs
+++ LongNumber.cs
@@ -4,70 +4,128 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LongArithmetics
 {
     public class LongNumber
     {
         const int BASE = 10;
-        bool Negative { get; set; }
+        bool Sign { get; set; }
         List<int> Digits { get; set; } = new List<int>();   // Reverse order!
-        int Length { get => Digits.Count; }
 
         public LongNumber() { }
 
         public LongNumber(string number)
         {
-            Negative = number[0] == '-' ? true : false;
+            Sign = number[0] == '-';
             for (int i = number.Length - 1; i > number.IndexOf('-'); i--)
                 Digits.Add(number[i] - '0');
         }
 
         public int this[int i]
         {
-            get => i < Length ? Digits[i] : 0;
+            get => i < Digits.Count ? Digits[i] : 0;
             set => Digits[i] = value;
         }
 
         public override string ToString()
         {
-            string result = Negative ? "-" : "";
-            for (int i = Length - 1; i >= 0; i--)
+            string result = Sign ? "-" : "";
+            for (int i = Digits.Count - 1; i >= 0; i--)
                 result += Digits[i];
             return result;
         }
 
+        public static implicit operator LongNumber(int n) => new LongNumber(n.ToString());
+
+        public static bool operator ==(LongNumber a, LongNumber b)
+        {
+            return a.Sign == b.Sign && a.Digits.SequenceEqual(b.Digits);
+        }
+
+        public static bool operator !=(LongNumber a, LongNumber b) => !(a == b);
+
+        public static bool operator <(LongNumber a, LongNumber b)
+        {
+            if (a.Sign != b.Sign) return a.Sign;
+
+            if (a.Digits.Count != b.Digits.Count)
+                return a.Sign ? a.Digits.Count > b.Digits.Count : a.Digits.Count < b.Digits.Count;
+
+            for (int i = a.Digits.Count; i >= 0; i--)
+                if (a[i] != b[i])
+                    return a.Sign ? a[i] > b[i] : a[i] < b[i];
+            
+            return false;
+        }
+
+        public static bool operator >(LongNumber a, LongNumber b)  => !(a <= b);
+
+        public static bool operator <=(LongNumber a, LongNumber b) => a == b || a < b;
+
+        public static bool operator >=(LongNumber a, LongNumber b) => !(a < b);
+
         public static LongNumber operator -(LongNumber a)
         {
-            return new LongNumber { Digits = a.Digits, Negative = !a.Negative };
+            return new LongNumber { Digits = a.Digits, Sign = !a.Sign };
         }
 
         public static LongNumber operator +(LongNumber a, LongNumber b)
         {
             var res = new LongNumber();
 
-            if (a.Negative == b.Negative)
+            if (a.Sign == b.Sign)
             {
                 int extra = 0;
-                res.Negative = a.Negative;
+                res.Sign = a.Sign;
 
-                for (int i = 0; i < Math.Max(a.Length, b.Length); i++)
+                for (int i = 0; i < Math.Max(a.Digits.Count, b.Digits.Count); i++)
                 {
                     int dSum = a[i] + b[i] + extra;
                     res.Digits.Add(dSum % BASE);
-                    extra = dSum / BASE == 0 ? 0 : 1;
+                    extra = dSum / BASE;
                 }
 
                 if (extra == 1) res.Digits.Add(1);
             }
-            else res = a.Negative ? b - (-a) : a - (-b);
-
+            else res = a.Sign ? b - (-a) : a - (-b);
             return res;
         }
 
         public static LongNumber operator -(LongNumber a, LongNumber b)
         {
-            throw new NotImplementedException();    // TODO
+            if (a == b) return 0;
+
+            var res = new LongNumber();
+            if (a.Sign == b.Sign)
+            {
+                int extra = 0;
+                int dDif;
+
+                if (a < b)
+                {
+                    for (int i = 0; i < Math.Max(a.Digits.Count, b.Digits.Count); i++)
+                    {
+                        dDif = a.Sign ? a[i] - b[i] - extra : b[i] - a[i] - extra;
+                        if (dDif < 0)
+                        {
+                            dDif += BASE;
+                            extra = 1;
+                        }
+                        else extra = 0;
+                        res.Digits.Add(dDif);
+                    }
+                    res.Sign = true;
+                }
+                else res = -(b - a);
+
+                int j = res.Digits.Count - 1;
+                while (res[j] == 0)
+                    res.Digits.RemoveAt(j--);
+            }
+            else res = a.Sign ? -(-a + b) : a + (-b);
+            return res;
         }
     }
 }
